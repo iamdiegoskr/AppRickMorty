f03a5bdf528e3321d71dac253c414526f80f18fd
diego martinez <juandiego337@hotmail.com>
2021-01-11 11:55:30 -0500

    fix some details

--- js/main.js
+++ js/main.js
@@ -1,22 +1,20 @@
 const getCharacteres = async() => {
-
     const BASE_API = 'https://rickandmortyapi.com/api/character/';
-
     try {
-
         let characteres = await fetch(BASE_API)
         return await characteres.json();
-
     } catch (error) {
         console.log(error)
     }
-
 }
 
 
 const printData = async() => {
 
     const characteres = await getCharacteres()
+
+
+
     console.log(characteres.results);
 
 }
